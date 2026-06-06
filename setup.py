8f9c30256dbe977118e1fd78811bf1e7bdf64f83
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2026-06-05 22:15:51 -0400

    chore: bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="pcapprocessor",
-    version="0.1.0",
+    version="1.0.0",
     description="A simple PCAP processor",
     author="Siddharth Gangadhar, Truc Anh N Nguyen, Santosh Gondi",
     author_email="lordlabakdas.code@gmail.com",
