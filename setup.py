d5ebe6f8ac0a7fe342cc305ba5ebd217a90a4a45
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2026-06-06 06:52:53 -0400

    chore: add python_requires and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -17,4 +17,15 @@ setup(
         "seaborn",
     ],
     packages=["pcapprocessor"],
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: System :: Networking :: Monitoring",
+    ],
 )
