de53daf557231b98597a7edd73b0c0a160b36e66
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2026-06-05 20:25:09 -0400

    refactor: remove legacy/ and old pbs/ files, export new classes from __init__

--- pcapprocessor/__init__.py
+++ pcapprocessor/__init__.py
@@ -1,3 +1,19 @@
 from pcapprocessor.processor import PcapProcessor
+from pcapprocessor.stats import ConfidenceInterval, MetricStats, XScaleArray
+from pcapprocessor.simulation import QueueSizeCalculator, WafCommandBuilder
+from pcapprocessor.trace import TraceProcessor
+from pcapprocessor.metrics import MetricsWriter
+from pcapprocessor.runner import BfsRunner, SimulationRunner
 
-__all__ = ["PcapProcessor"]
+__all__ = [
+    "PcapProcessor",
+    "ConfidenceInterval",
+    "MetricStats",
+    "XScaleArray",
+    "QueueSizeCalculator",
+    "WafCommandBuilder",
+    "TraceProcessor",
+    "MetricsWriter",
+    "BfsRunner",
+    "SimulationRunner",
+]
