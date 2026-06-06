5df7739720dbebdffe90065c174714ca9f13064f
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2026-06-06 06:53:22 -0400

    chore: bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pcapprocessor",
-    version="2.0.1",
+    version="2.0.2",
     description="A simple PCAP processor",
     author="Siddharth Gangadhar, Truc Anh N Nguyen, Santosh Gondi",
     author_email="lordlabakdas.code@gmail.com",
