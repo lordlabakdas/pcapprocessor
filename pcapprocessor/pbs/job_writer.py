7d12090e6f254b5d0cc4b9d5720ebb87f27d9090
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2026-06-05 20:16:40 -0400

    fix: use os.path.splitext and quote paths in PBS scripts

--- pcapprocessor/pbs/job_writer.py
+++ pcapprocessor/pbs/job_writer.py
@@ -16,7 +16,7 @@ class PbsJobWriter:
         self.jobs_dir = jobs_dir
         self.email = email
         self.work_dir = work_dir
-        self._cfg_name = config_file[:-4]
+        self._cfg_name = os.path.splitext(config_file)[0]
 
     def write(self) -> None:
         """Generate PBS job files for TCP variants × scenarios (was pbsWriter)."""
@@ -52,14 +52,14 @@ class PbsJobWriter:
             pbs_file.write("#PBS -m a\n")
             pbs_file.write("#PBS -N " + cfg_id + "\n\n")
             pbs_file.write("cd $PBS_O_WORKDIR\n")
-            pbs_file.write("cd " + self.work_dir + "\n")
-            pbs_file.write("mkdir " + run_dir + "\n")
-            pbs_file.write("cp " + self.config_file + " " + run_dir + "\n")
-            pbs_file.write("cd " + run_dir + "\n")
+            pbs_file.write('cd "' + self.work_dir + '"\n')
+            pbs_file.write('mkdir "' + run_dir + '"\n')
+            pbs_file.write('cp "' + self.config_file + '" "' + run_dir + '"\n')
+            pbs_file.write('cd "' + run_dir + '"\n')
             pbs_file.write(run_cmd + "\n")
-            pbs_file.write("mv *.csv *.mon " + self.work_dir + "\n")
-            pbs_file.write("cd " + run_dir + "\n")
-            pbs_file.write("rm -rf " + run_dir + "\n")
+            pbs_file.write('mv *.csv *.mon "' + self.work_dir + '"\n')
+            pbs_file.write('cd "' + run_dir + '"\n')
+            pbs_file.write('rm -rf "' + run_dir + '"\n')
 
     def _write_job(self, tcp: str, scen: str) -> str:
         unique_id = self._cfg_name + "_" + tcp + "_" + scen
@@ -79,13 +79,13 @@ class PbsJobWriter:
             pbs_file.write("#PBS -m abe\n")
             pbs_file.write("#PBS -N " + unique_id + "\n\n")
             pbs_file.write("cd $PBS_O_WORKDIR\n")
-            pbs_file.write("cd " + self.work_dir + "\n")
-            pbs_file.write("mkdir " + run_dir + "\n")
-            pbs_file.write("cp " + self.config_file + " " + run_dir + "\n")
-            pbs_file.write("cd " + run_dir + "\n")
+            pbs_file.write('cd "' + self.work_dir + '"\n')
+            pbs_file.write('mkdir "' + run_dir + '"\n')
+            pbs_file.write('cp "' + self.config_file + '" "' + run_dir + '"\n')
+            pbs_file.write('cd "' + run_dir + '"\n')
             pbs_file.write(run_cmd + "\n")
-            pbs_file.write("mv *.csv ../\n")
+            pbs_file.write('mv *.csv "../"\n')
             pbs_file.write("cd ..\n")
-            pbs_file.write("rm -rf " + run_dir + "\n")
+            pbs_file.write('rm -rf "' + run_dir + '"\n')
 
         return pbs_fi_name
